59bd373fc8d313a1b7dd9d4bac8581cd61e8f75f
jayasupriyasomasani <jayasupriya.somasani@narayanagroup.com>
2023-03-28 15:29:37 +0530

    1.Assets implemented

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,41 +10,49 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         height: 300,
-        child: ListView.builder(
-          itemBuilder: (ctx, index) {
-            return Card(
-                child: Row(
-              children: [
-                Container(
-                  margin:
-                      const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                  decoration: BoxDecoration(
-                      border: Border.all(width: 2, color: Colors.purple)),
-                  padding: const EdgeInsets.all(10),
-                  child: Text(
-                    '\$ ${_userTransactions[index].amount.toStringAsFixed(2)}',
-                    style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.purple),
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(_userTransactions[index].title,
-                        style: const TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold)),
-                    Text(
-                      DateFormat().format(_userTransactions[index].dateTime),
-                      style: const TextStyle(color: Colors.grey),
-                    )
-                  ],
-                )
-              ],
-            ));
-          },
-          itemCount: _userTransactions.length,
-        ));
+        child: _userTransactions.isEmpty
+            ? Column(
+                children: [
+                   Text("No transactions added yet"),
+                  Image.asset('assets/images/bird.jpg')
+                ],
+              )
+            : ListView.builder(
+                itemBuilder: (ctx, index) {
+                  return Card(
+                      child: Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.symmetric(
+                            vertical: 10, horizontal: 15),
+                        decoration: BoxDecoration(
+                            border: Border.all(width: 2, color: Colors.purple)),
+                        padding: const EdgeInsets.all(10),
+                        child: Text(
+                          '\$ ${_userTransactions[index].amount.toStringAsFixed(2)}',
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.purple),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(_userTransactions[index].title,
+                              style: const TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold)),
+                          Text(
+                            DateFormat()
+                                .format(_userTransactions[index].dateTime),
+                            style: const TextStyle(color: Colors.grey),
+                          )
+                        ],
+                      )
+                    ],
+                  ));
+                },
+                itemCount: _userTransactions.length,
+              ));
   }
 }
